3dea21400b03fa6637424c2f5a76ef286b415813
Scriptor <109689531+Scriptor1000@users.noreply.github.com>
2025-09-12 08:02:45 +0000

    Füge Methoden zur Überprüfung von Graph-Eigenschaften hinzu: Reflexivität, Symmetrie, Antisymmetrie und Transitivität

--- 25-08-29/graphen.py
+++ 25-08-29/graphen.py
@@ -31,6 +31,39 @@ class Graph:
     def get_degree(self, vertex) -> int:
         return len(self._graph[vertex]) + 1 if vertex in self._graph[vertex] else 0
 
+    def is_reflexive(self) -> bool:
+        return all(vertex in vertices for vertex, vertices in self._graph.items())
+        for vertex, vertices in self._graph.items():
+            if vertex not in vertices:
+                return False
+        return True
+
+    def is_symmetric(self) -> bool:
+        return all(vertex in self._graph[target] for vertex, vertices in self._graph.items() for target in vertices)
+        for vertex, vertices in self._graph.items():
+            for target in vertices:
+                if vertex not in self._graph[target]:
+                    return False
+        return True
+
+    def is_antisymmetric(self) -> bool:
+        return all(target == vertex or vertex not in self._graph[target] for vertex, vertices in self._graph.items() for target in vertices)
+        for vertex, vertices in self._graph.items():
+            for target in vertices:
+                if vertex in self._graph[target] and target != vertex:
+                    return False
+        return True
+
+    def is_transitiv(self) -> bool:
+        return all(t_target in vertices for vertex, vertices in self._graph.items() for target in vertices for t_target in self._graph[target])
+        for vertex, vertices in self._graph.items():
+            for target in vertices:
+                for t_target in self._graph[target]:
+                    if t_target not in vertices:
+                        return False
+        return True
+
+
     def __str__(self):
         s = ""
         for k, v in self._graph.items():
@@ -43,4 +76,7 @@ if __name__ == "__main__":
     c = Vertex("C")
     d = Vertex("D")
     g = Graph({a: [b], b: [b, c, d], c: [a], d: []})
-    print(g)
+    print(g.is_reflexive())
+    print(g.is_symmetric())
+    print(g.is_antisymmetric())
+    print(g.is_transitiv())
