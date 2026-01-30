5ec5f07f61502f7b794e7561fcad761ed9746c68
Scriptor <109689531+Scriptor1000@users.noreply.github.com>
2026-01-30 12:06:08 +0000

    Implementiere den Greedy Edge Coloring Algorithmus und aktualisiere die Gesamtfärbungsmethode

--- 26-01-30/coloring.py
+++ 26-01-30/coloring.py
@@ -21,6 +21,50 @@ def greedyColoring(inputGraph):
     
     return result
 
+def greedyEdgeColoring(inputGraph):
+    """
+    Funktion die die Nummer der Farbe von jeder Kante ausgibt mithilfe vom Greedy Edge Coloring Algorithmus
+    Beispiel Ausgabeformat: "(A, B): 0, (A, C): 1, ..."
+    """
+    result = {}
+    edges = set([tuple(sorted((u, v))) for u in inputGraph for v in inputGraph[u]])
+
+    for start, end in edges:
+        neighbore_edges = [edge for edge in edges if (edge[0] in [start, end] or edge[1] in [start, end]) and not edge == (start, end)]
+
+        neighbor_colors = set()
+        for neighbor in neighbore_edges:
+            if neighbor in result:
+                neighbor_colors.add(result[neighbor])
+
+        color = 0
+        while color in neighbor_colors:
+            color += 1
+
+        result[(start, end)] = color
+    return result
+
+
+def greedyTotalColoring(inputGraph):
+    result = greedyColoring(inputGraph)
+    edges = set([tuple(sorted((u, v))) for u in inputGraph for v in inputGraph[u]])
+
+    for start, end in edges:
+        neighbore_edges = [edge for edge in edges if (edge[0] in [start, end] or edge[1] in [start, end]) and not edge == (start, end)]
+
+        neighbor_colors = set([result[start], result[end]])
+        for neighbor in neighbore_edges:
+            if neighbor in result:
+                neighbor_colors.add(result[neighbor])
+
+        color = 0
+        while color in neighbor_colors:
+            color += 1
+
+        result[(start, end)] = color
+    return result
+
+
 if __name__ == "__main__":
     graph = {
         "A": ["B", "C", "D", "H"],
@@ -33,4 +77,4 @@ if __name__ == "__main__":
         "H": ["A", "B", "C", "D", "G"]
     }
     
-    print(f"Fertiger Gefärbter Graph: {greedyColoring(graph)}")
+    print(f"Fertiger Gefärbter Graph: {greedyTotalColoring(graph)}")
